a33c638159da36ee1c71ac27eb903d437437226b
Mikkel Lassen <Mikkelflassen15@gmail.com>
2024-02-15 13:44:42 +0100

    css updated, onClick, EventListerner

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 
 function hey() {
- console.log("hello world");
+    console.log("hello world from button 1");
 }
+
+function hej() {
+    console.log("hello world from button 2");
+}
+
+function hei() {
+    console.log("hello world from button 3");
+   }
+
+   document.getElementById("button").addEventListener("click", hey);
+   
